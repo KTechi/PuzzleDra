7c916a62fe8c93ad6650a7b45bdbf8366b4e7059
SoccerPlay32 <yahoosekainosekai@gmail.com>
2019-12-25 00:00:14 +0900

    入れ替えの処理実現

--- Executer.java
+++ Executer.java
@@ -5,6 +5,8 @@ public class Executer {
     boolean firstTouch;
     int stoneSize;
     int selectStone;
+    int prevX, prevY, nowX, nowY;
+
 
 	public Executer(int[][] field) {
 		this.field = field;
@@ -13,14 +15,31 @@ public class Executer {
 	}
 
 	void switchStone(double x, double y) {
+		int temp;
+
 		if(firstTouch) {
 			firstTouch = false;
 			/////xとyはwindowでの位置なので、
 			////実際のfieldの配列数と一致させる
 			selectX = (int)x/stoneSize;
 			selectY = (int)y/stoneSize;
+			prevX = selectX;
+			prevY = selectY;
 			selectStone = field[selectY][selectX];
 		}
+
+		prevX = nowX;
+		prevY = nowY;
+	    nowX = (int)x/stoneSize;
+		nowY = (int)y/stoneSize;
+
+		if(nowX != prevX || nowY != prevY) {
+			temp = field[prevY][prevX];//前にいる場所の石を保存
+			field[prevY][prevX] = field[nowY][nowX];//前の場所の石と今の場所の石を入れ替える
+			field[nowY][nowX] = temp;//今の場所に、前の場所の石を入れる。
+		}
+
+
 	}
 
 	void decide() {
