2bbb52d9e842fdb70ed536b82d3b40ef521558c9
KTeruuchi-S <cguc0087@outlook.jp>
2019-12-24 22:32:21 +0900

    [En] add Main and PuzzleCanvas [Ja] Main と PuzzleCanvas の追加

--- Main.java
+++ Main.java
@@ -1,4 +1,70 @@
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 
-public class Main {
+public class Main extends Application implements EventHandler {
+	// Components
+	private PreviewCanvas previewCanvas;
+	private PuzzleCanvas puzzleCanvas;
+	private Executer executer;
+	private boolean processing = false;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) {
+		// PreviewCanvas
+		previewCanvas = new PreviewCanvas();
+		
+		// PuzzleCanvas
+		puzzleCanvas = new PuzzleCanvas();
+		puzzleCanvas.addEventHandler(MouseEvent.ANY, this);
+		
+		// Executer
+		executer = new Executer(puzzleCanvas.field);
+		
+		// Pane
+		BorderPane bp = new BorderPane();
+		bp.setTop(previewCanvas);
+		bp.setCenter(puzzleCanvas);
+		
+		// Scene
+		Scene sc = new Scene(bp, 480, 900);
+
+		// Stage
+		primaryStage.setTitle("PuzzleDra");
+		primaryStage.setMinWidth(480);
+		primaryStage.setMaxWidth(480);
+		primaryStage.setMinHeight(725);
+		primaryStage.setMaxHeight(725);
+		primaryStage.setScene(sc);
+		primaryStage.show();
+	}
+
+	@Override
+	public void handle(Event event) {
+		if (!(event instanceof MouseEvent)) {
+			System.out.println("Error [PuzzleCanvasEventHandler]");
+			return;
+		}
+		MouseEvent e = (MouseEvent)event;
+		
+		if (e.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+			if (processing) return;
+			puzzleCanvas.XX = e.getX();
+			puzzleCanvas.YY = e.getY();
+			executer.switchStone(e.getX(), e.getY());
+		} else if (e.getEventType() == MouseEvent.MOUSE_RELEASED) {
+			processing = true;
+			executer.decide();
+			processing = false;
+		}
+
+		puzzleCanvas.paint();
+	}
 }
--- PuzzleCanvas.java
+++ PuzzleCanvas.java
@@ -1,4 +1,45 @@
+import javafx.scene.canvas.*;
+import javafx.scene.paint.Color;
 
-public class PuzzleCanvas {
-
+public class PuzzleCanvas extends Canvas {
+	GraphicsContext gc = getGraphicsContext2D();
+	private final int SIZE = 80;
+	int[][] field;
+	double XX = -1000, YY = -1000;
+	Color[] stoneColor = {
+			Color.YELLOW,
+			Color.GRAY,
+			Color.BLUE,
+			Color.RED,
+			Color.GREEN,
+			Color.PINK
+		};
+	
+	PuzzleCanvas() {
+		super(480, 500);
+		field = new int[5][6];
+		
+		for (int y = 0; y < field.length; y++)
+		for (int x = 0; x < field[0].length; x++) {
+			// make field
+			field[y][x] = (x + y) % 6;
+		}
+		paint();
+	}
+	
+	void paint() {
+		// set BackGround
+		gc.setFill(Color.BLACK);
+		gc.fillRect(0, 0, this.getWidth(), this.getHeight());
+		
+		for (int y = 0; y < field.length; y++)
+		for (int x = 0; x < field[0].length; x++) {
+			if (y == (int)YY / SIZE && x == (int)XX / SIZE) continue;
+			gc.setFill(stoneColor[field[y][x]]);
+			gc.fillOval(x * SIZE, y * SIZE, SIZE, SIZE);
+		}
+		if (YY <= -1 || XX <= -1) return;
+		gc.setFill(stoneColor[field[(int)YY/SIZE][(int)XX/SIZE]]);
+		gc.fillOval(XX-SIZE/2, YY-SIZE/2, SIZE, SIZE);
+	}
 }
