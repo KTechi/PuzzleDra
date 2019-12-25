a2d3f52da618e4b93279e780823616a8f0ca26f2
KTeruuchi-S <cguc0087@outlook.jp>
2019-12-25 19:33:42 +0900

    カーソルがキャンバス外に出た時の処理 executerの削除と落下処理

--- Executer.java
+++ Executer.java
@@ -52,7 +52,65 @@ public class Executer {
 		}
 	}
 
-	public boolean calculateStore() {
-		//TODO
+	public boolean calculateScore() {
+		boolean[][] deleteMask = new boolean[field.length][field[0].length];
+		boolean haveToDrop = false;
+		boolean haveToDelete = false;
+
+		for (int y = 0; y < field.length; y++)
+		for (int x = 0; x < field[0].length; x++) {
+			if (x + 1 < field[0].length && field[y][x] == field[y][x+1]) {
+				if (x + 2 < field[0].length && field[y][x] == field[y][x+2]) haveToDelete = true;
+				if (y + 1 < field.length    && field[y][x] == field[y+1][x]) haveToDelete = true;
+				if (y + 1 < field.length    && field[y][x] == field[y+1][x+1]) haveToDelete = true;
+			}
+			if (y + 1 < field.length && field[y][x] == field[y+1][x]) {
+				if (y + 2 < field.length    && field[y][x] == field[y+2][x]) haveToDelete = true;
+				if (x + 1 < field[0].length && field[y][x] == field[y+1][x+1]) haveToDelete = true;
+				if (0 <= x - 1              && field[y][x] == field[y+1][x-1]) haveToDelete = true;
+			}
+
+			if (haveToDelete) {
+				haveToDelete = false;
+				haveToDrop = true;
+
+				for (int i = 0; i < deleteMask.length; i++)
+				for (int j = 0; j < deleteMask[0].length; j++)
+					deleteMask[i][j] = false;
+
+				delete(x, y, deleteMask);
+
+				for (int i = 0; i < deleteMask.length; i++)
+				for (int j = 0; j < deleteMask[0].length; j++)
+					if (deleteMask[i][j])
+						field[i][j] = -1;
+			}
+		}
+		return haveToDrop;
+	}
+
+	private void delete(int x, int y, boolean[][] deleteMask) {
+		deleteMask[y][x] = true;
+
+		if (0 <= x - 1 && field[y][x] == field[y][x-1] && !deleteMask[y][x-1])              delete(x-1, y, deleteMask);
+		if (0 <= y - 1 && field[y][x] == field[y-1][x] && !deleteMask[y-1][x])              delete(x, y-1, deleteMask);
+		if (x + 1 < field[0].length && field[y][x] == field[y][x+1] && !deleteMask[y][x+1]) delete(x+1, y, deleteMask);
+		if (y + 1 < field.length    && field[y][x] == field[y+1][x] && !deleteMask[y+1][x]) delete(x, y+1, deleteMask);
+	}
+
+	public void dropStone() {
+		for (int x = 0; x < field[0].length; x++)
+		for (int i = 0; i < 5; i++)
+		for (int y = field.length-2; 0 <= y; y--) {
+			if (field[y+1][x] != -1) continue;
+			field[y+1][x] = field[y][x];
+			field[y][x] = -1;
+		}
+
+		for (int x = 0; x < field[0].length; x++)
+		for (int y = 0; y < field.length; y++) {
+			if (field[y][x] == -1)
+				field[y][x] = (int)(Math.random()*6);
+		}
 	}
 }
--- PuzzleCanvas.java
+++ PuzzleCanvas.java
@@ -16,11 +16,11 @@ public class PuzzleCanvas extends Canvas {
 			Color.GREEN,
 			Color.PINK
 		};
-	
+
 	PuzzleCanvas() {
 		super(480, 402);
+
 		field = new int[5][6];
-		
 		for (int y = 0; y < field.length; y++)
 		for (int x = 0; x < field[0].length; x++) {
 			// make field
@@ -28,44 +28,58 @@ public class PuzzleCanvas extends Canvas {
 		}
 		paint();
 	}
-	
+
 	void paint() {
 		// set BackGround
 		gc.setFill(Color.BLACK);
 		gc.fillRect(0, 0, this.getWidth(), this.getHeight());
-		
+
 		// print Stones
 		for (int y = 0; y < field.length; y++)
 		for (int x = 0; x < field[0].length; x++) {
-			// if the Stone is selected
+			// do not print -1
+			if (field[y][x] == -1) continue;
+			/////xとyはwindowでの位置なので、
+			////実際のfieldの配列数と一致させる
 			int stoneX = (int)XX / SIZE;
 			int stoneY = (int)YY / SIZE;
-			
+			// if the Stone is selected
+			// 選択している石なら continue;(表示しない)
 			if (y == stoneY && x == stoneX) continue;
-			if (y == stoneY && (XX<=1 || SIZE*field[0].length-1<XX)) continue;
-//			if (YY<=1 && y==0 && x==stoneX) continue;
-//			if (YY<=1 && y==0 && x==stoneX) continue;
-//			if (YY<=1 && y==stoneY && x==0) continue;
-//			if (YY<=1 && y==stoneY && x==0) continue;
-//			if () continue;
-//					
-//					|| SIZE*field.length-1<YY) && x == (int)XX / SIZE) continue;
-//			
-			// set Stone color & print
+
+			// if mouse is out of canvas keep select the nearest stone
+			//マウスカーソルがキャンバス外の時に、一番近い石を選択し続ける。(表示しない)
+			if (x==stoneX && YY<=1 && y==0) continue;//UP
+			if (x==stoneX && SIZE*field.length-1<YY && y==field.length-1) continue;//DOWN
+			if (y==stoneY && XX<=1 && x==0) continue;//LEFT
+			if (y==stoneY && SIZE*field[0].length-1<XX && x==field[0].length-1) continue;//RIGHT
+
+			if (y==0 && YY<=1 && x==0 && XX<=1) continue;//Left Up
+			if (y==0 && YY<=1 && x==field[0].length-1 && SIZE*field[0].length-1<=XX) continue;//Right Up
+			if (y==field.length-1 && SIZE*field.length-1<=YY && x==0 && XX<=1) continue;//Left Down
+			if (y==field.length-1 && SIZE*field.length-1<=YY && x==field[0].length-1 && SIZE*field[0].length-1<=XX) continue;//Right Down
+
 			gc.setFill(stoneColor[field[y][x]]);
 			gc.fillOval(x * SIZE, y * SIZE, SIZE, SIZE);
 		}
-		
+
 		// if is not pressing
-		if (!isPressing) return;
-		
+		if (!isPressing) {
+			if (field[0][0] == -1) return;
+			gc.setFill(stoneColor[field[0][0]]);
+			gc.fillOval(0, 0, SIZE, SIZE);
+			return;
+		}
+
+		// if XX out of canvas
 		if (XX <= 1) XX = 1;
 		else if (SIZE*field[0].length-1 < XX) XX = SIZE*field[0].length-1;
-
+		// if YY out of canvas
 		if (YY <= 1) YY = 1;
 		else if (SIZE*field.length-1 < YY) YY = SIZE*field.length-1;
-		
+
 		// set selected Stone
+		//選択した石の描画
 		gc.setFill(stoneColor[field[(int)YY/SIZE][(int)XX/SIZE]]);
 		gc.fillOval(XX-SIZE/2, YY-SIZE/2, SIZE, SIZE);
 	}
